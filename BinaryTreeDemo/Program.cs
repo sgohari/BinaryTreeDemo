51d2fadf3779801d30ba09dbe695cc20f1539f09
Nasir <31908515+sgohari@users.noreply.github.com>
2018-04-12 11:13:22 -0400

    Binary Tree in C#

--- BinaryTreeDemo/Program.cs
+++ BinaryTreeDemo/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace BinaryTreeDemo
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
+            //created random number generator.
+            int num;
+
+            Random rnd = new Random();
+            num = rnd.Next(0, 100);
+
+           Tree Mytree = new Tree(num);
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            string myString = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                num = rnd.Next(0, 100);
+                Mytree.Add(n);
+                Mytree.Print(null, ref myString);
+                Console.WriteLine(myString);
+            }
+           
+           
+            Console.ReadLine(); // run it now// exactly same but works different/
+            // thats ok man....
         }
     }
 }
---
+++ BinaryTreeDemo/Tree.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeDemo
+{
+    class Tree
+    {
+        Node top;
+
+        public Tree()
+        {
+            top = null;
+        }
+
+        public Tree(int initial)
+        {
+            top = new Node(initial);
+        }
+
+        public void Add(int value)
+        {
+            //non-recurse add
+            if (top == null)// the tree is empty.
+            {
+                //add item as the base node
+                Node newNode = new Node(value);
+                top = newNode;
+                return;
+            }
+            Node currentnode = top;
+            bool added = false;
+
+            do
+            {
+                //traerse tree
+                if (value < currentnode.value)
+                    //go to left
+                    if (currentnode.left == null)
+                    {
+                        //add the item
+                        Node NewNode = new Node(value);
+                        added = true;
+                    }
+                    else
+                    {
+                        //go left
+                        currentnode = currentnode.left;
+                    }
+
+                if (value >= currentnode.value)
+                {
+                    if (currentnode.right == null)
+                    {
+                        Node NewNode = new Node(value);
+                        currentnode.right = NewNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        //go right
+                        currentnode = currentnode.right;
+                    }
+                }
+            } while (!added);
+
+    
+        }
+
+        public void AddRc(int value)
+        {
+            //recurse add
+            AddR(ref top, value);
+
+        }
+
+        private void AddR(ref Node N, int value)
+        {
+            //private recursive search where add the new mode
+            if (N == null)
+            {
+                //Node doesn't exist add it here
+                Node NewNode = new Node(value);
+                //Set the old Node refeence to the newly created mode, Attach it to the tree
+                N = NewNode;
+                return; // End the function call and fall back
+            }
+            if (value < N.value)
+            {
+                AddR(ref N.left, value);
+                return;
+            }
+            if (value > N.value)
+            {
+                AddR(ref N.right, value);
+                return;
+            }
+        }
+
+
+        public void Print( Node N,ref string s)
+        {
+            //Write out the tree in sorted order othe strig newstring
+           
+            //implement using reursion
+            if (N == null) { N = top; }
+
+            if (N.left != null)
+            {
+                Print(N.left, ref s);
+               // s = s + N.value.ToString().PadLeft(3);
+            }
+            else
+            {
+                s = s = N.value.ToString().PadLeft(3);
+            } 
+            if (N.right != null)
+            {
+                Print(N.right, ref s);
+            }
+        }
+    }
+}
